2bf5dac94a63af89a873001766efdf3c2704fb08
abubakar <abubakaar@gmail.com>
2024-04-21 17:38:33 +0500

    adding new forms

--- buy.js
+++ buy.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('myForm').addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the form from submitting
+        event.preventDefault(); 
 
         // Get form values
-        var firstName = document.getElementById('first-name').value.trim();
-        var lastName = document.getElementById('last-name').value.trim();
-        var address = document.getElementById('address').value.trim();
-        var postalCode = document.getElementById('postal-code').value.trim();
-        var phone = document.getElementById('phone').value.trim();
+        var firstName = document.getElementById('first-name')
+        var lastName = document.getElementById('last-name')
+        var address = document.getElementById('address')
+        var postalCode = document.getElementById('postal-code')
+        var addres = document.getElementById('inputAddress2')
+        var city = document.getElementById('inputCity')
+        var state = document.getElementById('inputState')
 
         // Check if all fields are filled
-        if (firstName !== '' && lastName !== '' && address !== '' && postalCode !== '' && phone !== '') {
+        if (firstName !== '' && lastName !== '' && address !== '' && postalCode !== '' && addres !== '' && city !== '' && state !== '') {
             // Save user information in localStorage
             localStorage.setItem('firstName', firstName);
             localStorage.setItem('lastName', lastName);
             localStorage.setItem('address', address);
             localStorage.setItem('postalCode', postalCode);
-            localStorage.setItem('phone', phone);
-
+            localStorage.setItem('addres', addres);
+            localStorage.setItem('city', city);
+            localStorage.setItem('state', state);
             // Redirect to index.html
             window.location = 'index.html';
             // Show alert
-            alert('Your order is placed');
+            alert('Your order is placed').requird();
         } else {
             // If any field is empty, show an alert
             alert('Please fill in all fields.');
